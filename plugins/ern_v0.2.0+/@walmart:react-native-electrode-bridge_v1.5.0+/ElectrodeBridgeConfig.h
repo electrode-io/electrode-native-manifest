a0819c205ff8ad30856acda53b1c6c636bd4071f
w0l00qx <wli@walmartlabs.com>
2017-07-06 15:22:58 -0700

    ios: added electrodebridgeconfig

---
+++ plugins/ern_v0.2.0+/@walmart:react-native-electrode-bridge_v1.5.0+/ElectrodeBridgeConfig.h
@@ -0,0 +1,17 @@
+//
+//  ElectrodeBridgePluginConfig.h
+//  ElectrodeContainer
+//
+//  Created by Claire Weijie Li on 7/6/17.
+//  Copyright © 2017 Walmart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ElectrodeReactNative.h"
+
+
+@interface ElectrodeBridgeConfig: NSObject<ElectrodePluginConfig>
+@property (nonatomic, assign) BOOL debugEnabled;
+@property (nonatomic, strong) NSURL *jsBundleURL;
+- (instancetype)initWithDebugEnabled: (BOOL)enabled;
+@end
