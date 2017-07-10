6bd3e2a8102eaa56f87db99f62b27b925876dc01
w0l00qx <wli@walmartlabs.com>
2017-07-10 16:05:22 -0700

    Added ElectrodeMapsConfig.h and .m

---
+++ plugins/ern_v0.2.0+/react-native-maps_v0.15.2+/ElectrodeMapsConfig.h
@@ -0,0 +1,16 @@
+//
+//  ElectrodeMapsConfig.h
+//  ElectrodeContainer
+//
+//  Created by Claire Weijie Li on 6/27/17.
+//  Copyright © 2017 Walmart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ElectrodeReactNative.h"
+NS_ASSUME_NONNULL_BEGIN
+@interface ElectrodeMapsConfig : NSObject<ElectrodePluginConfig>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ plugins/ern_v0.2.0+/react-native-maps_v0.15.2+/ElectrodeMapsConfig.m
@@ -0,0 +1,11 @@
+//
+//  ElectrodeMapsConfig.m
+//  ElectrodeContainer
+//
+//  Created by Claire Weijie Li on 6/27/17.
+//  Copyright © 2017 Walmart. All rights reserved.
+//
+
+#import "ElectrodeMapsConfig.h"
+#import <AirMaps/AIRMaps.h>
+
