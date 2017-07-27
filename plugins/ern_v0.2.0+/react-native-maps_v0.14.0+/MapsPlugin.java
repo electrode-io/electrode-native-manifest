6f89441f4698a620f5b931d0a94717fc5d7d82f8
Benoit Lemaire <blemaire@walmartlabs.com>
2017-07-27 15:05:14 -0700

    Fix react-native-maps config for 0.13.1

---
+++ plugins/ern_v0.2.0+/react-native-maps_v0.14.0+/MapsPlugin.java
@@ -0,0 +1,18 @@
+package com.walmartlabs.ern.container.plugins;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+
+import com.facebook.react.ReactInstanceManagerBuilder;
+import com.facebook.react.ReactPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+
+public class MapsPlugin {
+
+    public ReactPackage hook(@NonNull Application application,
+                     @NonNull ReactInstanceManagerBuilder reactInstanceManagerBuilder) {
+        MapsPackage mapsPackage = new MapsPackage();
+        reactInstanceManagerBuilder.addPackage(mapsPackage);
+        return mapsPackage;
+    }
+}
