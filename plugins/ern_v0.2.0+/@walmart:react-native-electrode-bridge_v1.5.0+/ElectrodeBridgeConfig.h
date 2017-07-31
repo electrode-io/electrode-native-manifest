4f7de1230b01ed45c626ef8fcb85be1df14749b0
Claire Weijie Li <WLi@walmartlabs.com>
2017-07-31 12:54:33 +0800

    Implement ElectrodePluginConfig

--- plugins/ern_v0.2.0+/@walmart:react-native-electrode-bridge_v1.5.0+/ElectrodeBridgeConfig.h
+++ plugins/ern_v0.2.0+/@walmart:react-native-electrode-bridge_v1.5.0+/ElectrodeBridgeConfig.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-
-@interface ElectrodeBridgeConfig : NSObject
+#import "ElectrodeReactNative.h"
+@interface ElectrodeBridgeConfig : NSObject<ElectrodePluginConfig>
 
 @end
