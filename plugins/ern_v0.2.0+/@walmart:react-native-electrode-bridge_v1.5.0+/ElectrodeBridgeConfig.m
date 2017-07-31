29260eefa93f05a621a3e2cd42d0947ccf737b91
Claire Weijie Li <WLi@walmartlabs.com>
2017-07-31 12:55:07 +0800

    Implement ElectrodePluginConfig in .m

--- plugins/ern_v0.2.0+/@walmart:react-native-electrode-bridge_v1.5.0+/ElectrodeBridgeConfig.m
+++ plugins/ern_v0.2.0+/@walmart:react-native-electrode-bridge_v1.5.0+/ElectrodeBridgeConfig.m
@@ -7,7 +7,15 @@
 //
 
 #import "ElectrodeBridgeConfig.h"
-
+#if __has_include(<React/RCTBridgeDelegate.h>)
+#import <React/RCTBridgeDelegate.h>
+#elif __has_include("RCTBridgeDelegate.h")
+#import "RCTBridgeDelegate.h"
+#else
+#import "React/RCTBridgeDelegate.h"   // Required when used as a Pod in a Swift project
+#endif
 @implementation ElectrodeBridgeConfig
+- (void)setupConfigWithDelegate: (id<RCTBridgeDelegate>) delegate {
 
+}
 @end
