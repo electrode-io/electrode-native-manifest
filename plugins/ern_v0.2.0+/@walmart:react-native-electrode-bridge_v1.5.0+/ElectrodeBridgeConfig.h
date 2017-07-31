7c78664fd712105663c8a3dfb4502c4ac877620c
w0l00qx <wli@walmartlabs.com>
2017-07-31 12:44:52 +0800

    ios: Added ElectrodeBridgeConfig

---
+++ plugins/ern_v0.2.0+/@walmart:react-native-electrode-bridge_v1.5.0+/ElectrodeBridgeConfig.h
@@ -0,0 +1,13 @@
+//
+//  ElectrodeBridgeConfig.h
+//  ErnRunner
+//
+//  Created by Claire Weijie Li on 7/31/17.
+//  Copyright © 2017 Claire Weijie Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ElectrodeBridgeConfig : NSObject
+
+@end
---
+++ plugins/ern_v0.2.0+/@walmart:react-native-electrode-bridge_v1.5.0+/ElectrodeBridgeConfig.m
@@ -0,0 +1,13 @@
+//
+//  ElectrodeBridgeConfig.m
+//  ErnRunner
+//
+//  Created by Claire Weijie Li on 7/31/17.
+//  Copyright © 2017 Claire Weijie Li. All rights reserved.
+//
+
+#import "ElectrodeBridgeConfig.h"
+
+@implementation ElectrodeBridgeConfig
+
+@end
