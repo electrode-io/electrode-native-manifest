2fa537578661deb9e9b6bab6e5afb290aa7f2d8e
Benoit Lemaire <lemaireb@gmail.com>
2017-11-22 10:14:48 -0800

    Create VectorIconsPlugin.java

---
+++ plugins/ern_v0.5.0+/react-native-vector-icons_v4.0.0+/VectorIconsPlugin.java
@@ -0,0 +1,17 @@
+package com.walmartlabs.ern.container.plugins;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+
+import com.facebook.react.ReactInstanceManagerBuilder;
+import com.facebook.react.ReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+
+public class VectorIconsPlugin {
+    public ReactPackage hook(@NonNull Application application,
+                     @NonNull ReactInstanceManagerBuilder reactInstanceManagerBuilder) {
+        VectorIconsPackage vectorIconsPackage = new VectorIconsPackage();
+        reactInstanceManagerBuilder.addPackage(vectorIconsPackage);
+        return vectorIconsPackage;
+    }
+}
