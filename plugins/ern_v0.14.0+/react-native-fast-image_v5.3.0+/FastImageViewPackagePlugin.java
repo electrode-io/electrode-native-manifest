a33963606a95d6dd05ed9e5d97482b1097ec5a06
tianling536 <tianling536@gmail.com>
2019-06-03 19:15:50 +0800

    Remove android support-v4 from config file and unused import class.

--- plugins/ern_v0.14.0+/react-native-fast-image_v5.3.0+/FastImageViewPackagePlugin.java
+++ plugins/ern_v0.14.0+/react-native-fast-image_v5.3.0+/FastImageViewPackagePlugin.java
@@ -3,8 +3,6 @@ package com.walmartlabs.ern.container.plugins;
 import android.app.Application;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-
-import com.facebook.react.ReactInstanceManagerBuilder;
 import com.facebook.react.ReactPackage;
 import com.dylanvann.fastimage.FastImageViewPackage;
 
