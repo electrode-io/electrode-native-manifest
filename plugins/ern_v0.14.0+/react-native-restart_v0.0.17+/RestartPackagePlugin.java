91bc8fef63e62ad4a99938ecbcf2321f97438538
Frieder Bluemle <frieder.bluemle@walmartlabs.com>
2021-02-05 02:15:26 -0800

    Add react-native-restart

---
+++ plugins/ern_v0.14.0+/react-native-restart_v0.0.17+/RestartPackagePlugin.java
@@ -0,0 +1,14 @@
+package com.walmartlabs.ern.container.plugins;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.facebook.react.ReactPackage;
+import com.reactnativerestart.RestartPackage;
+
+public class RestartPackagePlugin implements ReactPlugin {
+    public ReactPackage hook(@NonNull Application application, @Nullable ReactPluginConfig config) {
+        return new RestartPackage();
+    }
+}
